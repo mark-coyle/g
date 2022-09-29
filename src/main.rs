306c24e84153f0cb2261a5c6227dfb46b9e491fd
Mark Coyle <mark.coyle@bambooloans.com>
2022-09-29 17:00:51 +0100

    (feat) Add simple diff stat to table

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,29 @@
-use cli_table::Color::{Green, Blue};
+use cli_table::Color::{Green, Blue, Yellow};
 
 use std::{fs, process::{Command, Output}};
 
-use cli_table::{format::Justify, print_stdout, Cell, Style, Table};
+use cli_table::{print_stdout, Cell, Style, Table};
 
 
 #[derive(Table)]
 struct Repo {
-    #[table(title = "Repo", justify = "Justify::Right")]
+    #[table(title = "Repo")]
     #[table(color = "Blue")]
     repo: String,
     #[table(title = "Branch name")]
     #[table(color = "Green")]
     branch_name: String,
+    #[table(title = "Diff")]
+    #[table(color = "Yellow")]
+    diff: String
 }
 
 fn main() {
-
     let table = build_table_from_repos().table()
                                         .title(vec![
                                             "Repo".cell().bold(true),
                                             "Current branch".cell().bold(true),
+                                            "Diff".cell().bold(true),
                                         ])
                                         .bold(true);
     assert!(print_stdout(table).is_ok());
@@ -35,10 +38,13 @@ fn build_table_from_repos() -> Vec<Repo> {
         let repo_name = path_name.to_str().unwrap().split("/").last().unwrap();
         let branch = current_branch_for_repo(path_name.to_str().unwrap());
         let branch_name = String::from_utf8(branch.stdout).unwrap();
+        let diff = String::from_utf8(diff_for_repo_branch(repo_name).stdout).unwrap();
+        let diff_empty = diff.is_empty();
 
         let repo = Repo {
             repo: repo_name.to_string(),
-            branch_name
+            branch_name,
+            diff: if diff_empty { "None".to_string() } else { diff.to_string() }
         };
 
         table.push(repo);
@@ -51,6 +57,14 @@ fn current_branch_for_repo(repo_path: &str) ->  Output {
     let res = Command::new("git").args(["-C", repo_path, "rev-parse", "--abbrev-ref", "HEAD"])
         .output()
         .expect("Error when determining branch");
+
+    return res
+}
+
+fn diff_for_repo_branch(repo_path: &str) ->  Output {
+    let res = Command::new("git").args(["-C", repo_path, "diff", "--shortstat"])
+        .output()
+        .expect("Error when fetching git diff");
 
     return res
 }
